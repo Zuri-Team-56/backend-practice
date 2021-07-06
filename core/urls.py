6723e2938ddc7ea0edef35028d4b79a9da0df20c
Poshpeck <egbohpaul@gmail.com>
2021-07-06 13:09:40 +0100

    Add API Endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path, include
-# from .views import DetailCareerQuestion, ListStudentAccount, DetailStudentAccount, ListCareerQuestion, DetailCareerQuestion
-
-# urlpatterns = [
-#     path('<int:pk>/', DetailStudentAccount.as_view()),
-#     path('', ListStudentAccount.as_view()),
-#     path('careerquestion/', ListCareerQuestion.as_view()),
-#     path('detailcareerquestion/', DetailCareerQuestion.as_view())
-# ]
-
 from django.urls import include, path
 from rest_framework import routers
 from . import views
@@ -22,11 +12,9 @@ router.register(r'savedcourses', views.SavedCourseViewSet)
 router.register(r'savedscholarships', views.SavedScholarshipViewSet)
 router.register(r'careerquestionoptions', views.CareerQuestionOptionViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 GET_OPTIONS = {'get': 'list'}
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('careerquestions/', views.CareerQuestionViewSet.as_view(GET_OPTIONS), name='careerquestions')
 ]
