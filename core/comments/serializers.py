97169cf22a52e0063231fcfd728bf58e9a7656d2
pauline-banye <paulinebanye@gmail.com>
2021-07-16 13:21:35 +0100

    endpoint edits

--- core/comments/serializers.py
+++ core/comments/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
 from .models import Comment
-
+# from core.articles.serializers import ArticleSerializer
+# from core.student.serializers import StudentAccountSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
     article = serializers.SlugRelatedField(read_only=True, slug_field='title')
+    # article = ArticleSerializer(many=True, read_only=True)
+    # commenter = StudentAccountSerializer(many=True, read_only=True)
     commenter = serializers.SlugRelatedField(read_only=True, slug_field='slug')
 
     class Meta:
